93a77a9c2cc7a9fa2c85dd2a47e197912c0132f2
조수현 <96sia@naver.com>
2021-12-22 17:58:16 +0900

    아두이노 서버당

---
+++ project_dependo/src/Arduino/GetArduino.java
@@ -0,0 +1,25 @@
+package Arduino;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/GetArduino")
+public class GetArduino extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArduinoDAO dao = new ArduinoDAO();
+		ArduinoVO vo = dao.getSensor();	
+		String result = new Gson().toJson(vo);	
+		PrintWriter out = response.getWriter();
+		out.print(result);
+	}
+}
